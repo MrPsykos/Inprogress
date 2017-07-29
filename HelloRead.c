3ba4b1a0e9aefb685f4a1bb08815d535ac65bcd4
MrPsykos <Yusof_97@hotmail.com>
2017-07-29 19:04:11 +0200

    Add files via upload

--- HelloRead.c
+++ HelloRead.c
@@ -1,35 +1,44 @@
 #include <stdio.h>
 #include <string.h>
-#define WORDLENGTH 30
-#define NROFSTRINGS 10000
-void readFromFile(char fileName[],char strings[WORDLENGTH][NROFSTRINGS]);
+#include <stdlib.h>
+#define WORDLENGTH 300000
+#define NROFSTRINGS 300
+void readFromFile(char fileName[],char *words[WORDLENGTH], int *readStrings);
 int main()
 {
-	    char fileName[WORDLENGTH];
-	    char strings[WORDLENGTH][NROFSTRINGS];	
-	    readFromFile(fileName, strings[NROFSTRINGS]);
-	    for(int i = 0; i < NROFSTRINGS-1;i++)
-	    {
-	    	printf("%s\n",strings[i]);
-	    }
-	
-	return 0;
-}
-void readFromFile(char fileName[], char strings[WORDLENGTH][NROFSTRINGS])
-{
-	printf("Hello");
-	sprintf(fileName,"data.txt",fileName);
-	
-	FILE *fp;
-	fp=fopen(fileName,"r");
-	if(fp!=NULL)
+    int readStrings = 0;
+    char fileName[WORDLENGTH];
+    char *words[WORDLENGTH];
+    char **pointer2words = words;
+    for (int i=0 ; i<WORDLENGTH; i++) {
+        if ((words[i] = malloc(sizeof(char) * NROFSTRINGS)) == NULL) {
+            printf("unable to allocate memory \n");
+            return -1;
+        }
+    }
+
+
+    readFromFile(fileName, words, &readStrings);
+    printf("%d\n", readStrings);
+    for(int i = 0; i< readStrings ; i++)
     {
-    	int i = 0;
-    	char a[WORDLENGTH];
-    	while(fscanf(fp,"%s",a != '\0'))
-    	{
-    		strings[i][0] = a;
-    		i++;
-    	}
+        printf("%s",words[i]);
+    }
+
+}
+void readFromFile(char fileName[], char *words[WORDLENGTH], int *readStrings)
+{
+    fileName = "data3.txt";
+    FILE *fp;
+    fp=fopen(fileName,"r");
+    if(fp!=NULL)
+    {
+        int i = 0;
+        while(fscanf(fp,"%s",words[i])==1)
+        {
+            strcat(words[i], "  ");
+            i++;
+            (*readStrings)++;
+        }
     }
 }
